0915e8422f749b00c69047323175f8f58ba5e86d
Tomiwa Adelae <thetommedia@gmail.com>
2025-06-19 17:14:05 +0100

    Updated the spaces listing and used the icons instead of the space photo

--- components/WorkSpaces.tsx
+++ components/WorkSpaces.tsx
@@ -12,7 +12,38 @@ export const WorkSpaces = ({ spaces }: { spaces: ISpace[] }) => {
 					Redefining the Future of Workspaces{" "}
 				</h2>
 
-				<div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-8">
+				<div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-8">
+					{spaces?.map((space, index) => (
+						<Link
+							key={index}
+							// @ts-ignore
+							href={space?._id!}
+							className="shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] p-6 rounded-xl hover:bg-[#F7F7F7] transition-all hover:border-secondary border-2 border-transparent"
+						>
+							<Image
+								src={
+									// @ts-ignore
+									space?.category?.image! ||
+									DEFAULT_SPACE_IMAGE
+								}
+								// @ts-ignore
+								alt={space?.category?.name || "Space icon"}
+								width={1000}
+								height={1000}
+								className="size-[100px] object-cover"
+							/>
+							<h4 className="font-medium text-lg underline text-secondary mt-2.5">
+								{/* @ts-ignore */}
+								{space?.category?.name!}
+							</h4>
+							<p className="mt-3 text-sm md:text-base text-muted-foreground leading-relaxed line-clamp-2">
+								{space?.description}
+							</p>
+						</Link>
+					))}
+				</div>
+
+				{/* <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-8">
 					{spaces?.map((space, index) => {
 						const coverPhoto =
 							// @ts-ignore
@@ -49,7 +80,7 @@ export const WorkSpaces = ({ spaces }: { spaces: ISpace[] }) => {
 							</Link>
 						);
 					})}
-				</div>
+				</div> */}
 			</div>
 
 			{/* <div className="mt-8">
